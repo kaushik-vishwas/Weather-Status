b4daad491300a4db38c747f31daf4543a41d2690
Kaushik <kaushikbiswascse@gmnail.com>
2023-08-08 17:16:27 +0530

    first commit

---
+++ index.js
@@ -0,0 +1,34 @@
+const http = require('http');
+const fs = require('fs');
+var requests = require("requests");
+
+// const httpRequests = require('requests');
+
+const homeFile  = fs.readFileSync("home.html", "utf-8");
+const replaceVal = (tempVal, orgVal) => {
+    let temperature = tempVal.replace("{%tempval%}", orgVal.main.temp);
+    temperature = temperature.replace("{%tempmin%}", orgVal.main.temp_min);
+    temperature = temperature.replace("{%tempmax%}", orgVal.main.temp_max);
+    temperature = temperature.replace("{%location%}", orgVal.name);
+    temperature = temperature.replace("{%country%}", orgVal.sys.country);
+    return temperature;
+
+}
+const server = http.createServer((req, res)=>{
+    if(req.url == "/"){
+        requests('http://api.openweathermap.org/data/2.5/weather?q=Pune&appid=1bf7a4a3f75503d6678cdb40a7d25053')
+        .on('data', function (chunk) {
+            const objdata = JSON.parse(chunk);
+            const arrData = [objdata];
+          console.log(arrData[0].main.temp);
+          const realTimeData = arrData.map(val => replaceVal(homeFile, val)).join("");
+          res.write(realTimeData);
+          console.log(realTimeData);
+        })
+        .on('end', function (err) {
+          if (err) return console.log('connection closed due to errors', err);
+          res.end();
+        });
+    }   
+});
+ server.listen(8000, "127.0.0.1");
